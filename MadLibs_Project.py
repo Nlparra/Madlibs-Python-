714e66cbf20863b29526544e2fcc73708b802200
Nelson Parra <nlparra.dev@gmail.com>
2020-04-23 13:19:11 -0400

    finish working the code , need improving on content

--- MadLibs_Project.py
+++ MadLibs_Project.py
@@ -1,19 +1,27 @@
-print("I stay out too late, got" + {verb1} + "in my brain")
-print("That's what" + {subject1} + "say, mmm hmm, that's what" + {subject2} + "say, mmm hmm")
-print("I go on too many" + {place1}, + " but I can't make 'em stay")
-print("That's what" + {subject3} + "say, mmm hmm, that's what" + {subject4} + "say, mmm hmm")
-print("But I keep" + {word1}, + " can't stop, won't stop " + {word2})
-print("It's like I got this" + {thing1} + " in my mind, sayin " + {funny1})
-print("'Cause the players gonna" + {funny2} + {funny3} +  {funny4} + {funny5} + {funny6})
-print("hello world")
+verb1 = input("please write a verb")
+subject1 = input("please write a subject")
+subject2 = input("please write a subject")
+place1 = input("please write a funny object")
+subject3 = input("please write a subject")
+subject4 = input("please write a subject")
+word1 = input("please write a funny word")
+word2 = input("please write a funny word")
+thing1 = input("please write a thing")
+funny1 = input("please write a funny word")
+funny2 = input("please write a funny word")
+funny3 = input("please write a funny word")
+funny4 = input("please write a funny word")
+verb2 = input("please write a verb")
 
+print("I stay out too late, got" + (verb1) + " " + "in my brain")
+print("That's what" + " " + (subject1) + " " + "say, mmm hmm, that's what" + " " + (subject1) + " " + "say, mmm hmm")
+print("I go on too many" + " " + (place1) + " " + "but I can't make 'em stay")
+print("That's what the" + (subject3) + " " + "say, mmm hmm, that's what" + " " + (subject4) + " " + "say, mmm hmm")
+print("But I keep" + " " + (word1) + " " + "can't stop, won't stop " + " " + (word2) )
+print("It's like I got this" + (thing1) + "in my mind, sayin" + (funny1) + " ")
+print("'Cause the players gonna" + " " + (funny2) + " " + (funny2) + " " + (funny2) + " " + (funny2) + " " + (funny2) + " ")
+print("And the haters gonna" + " " + (funny3) + " " + (funny3) + " " +  (funny3) + " " +(funny3) + " " + (funny3) + " ")
+print("'Cause the players gonna" + (funny4) + " " + (funny4) + " " +  (funny4) + " " + (funny4) + " " + (funny4))
+print("I" + (verb2) + "it off, I" + " " + (verb2) + " " + "it off")
 
-# 'Cause the players gonna play, play, play, play, play
-# And the haters gonna hate, hate, hate, hate, hate
-# Baby, I'm just gonna shake, shake, shake, shake, shake
-# I shake it off, I shake it off
-# Heartbreakers gonna break, break, break, break, break
-# And the fakers gonna fake, fake, fake, fake, fake
-# Baby, I'm just gonna shake, shake, shake, shake, shake
-# I shake it off, I shake it off
 
